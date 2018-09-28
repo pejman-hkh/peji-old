86b0d114289804708d3a99aa2c2df1ea3b83eb97
pejman hkh <pejman.hkh@gmail.com>
2018-09-28 19:36:02 +0330

    add controller to peji lib

--- app/Controller/baseController.php
+++ app/Controller/baseController.php
@@ -1,25 +1,10 @@
 <?
 namespace App\Controller;
 
-use Pejman\Singleton as Singleton;
+use Peji\Controller as Controller;
 
-class baseController extends Singleton {
+class baseController extends Controller {
 
-	protected $set;
-
-	protected function set( $key, $val = [] ) {
-		if( is_array( $key ) ) {
-			foreach( $key as $k => $v ) {
-				$this->set[ $k ] = $v;
-			}
-		} else {
-			$this->set[ $key ] = $val;
-		}
-	}
-
-	protected function get() {
-		return $this->set;
-	}
 }
 
 ?>
---
+++ peji/Controller.php
@@ -0,0 +1,22 @@
+<?php
+namespace Peji;
+use Pejman\Singleton;
+
+class Controller extends Singleton {
+
+	protected $set;
+
+	protected function set( $key, $val = [] ) {
+		if( is_array( $key ) ) {
+			foreach( $key as $k => $v ) {
+				$this->set[ $k ] = $v;
+			}
+		} else {
+			$this->set[ $key ] = $val;
+		}
+	}
+
+	protected function get() {
+		return $this->set;
+	}
+}
