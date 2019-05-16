816b56a358d3e798a3f9d732ae403b57b10e1401
pejman hkh <pejman-hkh@users.noreply.github.com>
2019-05-16 11:23:56 +0430

    Update Singleton.php

--- peji/Singleton.php
+++ peji/Singleton.php
@@ -8,8 +8,8 @@ class Singleton {
 	public static function __callStatic($method, $args) {
 		$className = get_called_class();
 
-    	return call_user_func_array( array( self::$Classes[ $className ] = @self::$Classes[ $className ]?:( class_exists( $className ) ? new $className() : false ), $method ), $args );
-    }
+		return call_user_func_array( array( self::$Classes[ $className ] = @self::$Classes[ $className ]?:( class_exists( $className ) ? new $className() : false ), $method ), $args );
+	}
 
 
 	protected function getInstance() {
